5867760083992bd8910e3d1646b8c276b193a150
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-30 23:30:09 +0200

    02 basics

---
+++ 02_basics/script.py
@@ -0,0 +1,32 @@
+"""
+- valid variables name
+- invalid variables name
+- reserved keywords
+- expression VS statement
+
+"""
+
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# This is a comment
+
+'''
+This is document comment
+
+********************
+*                  *
+*     WELCOME!     *
+*                  *
+********************
+'''
