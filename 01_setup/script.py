ae305b689e230a56079a81dacbcb91c4a43e1a82
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-30 23:29:58 +0200

    01 setup

---
+++ 01_setup/script.py
@@ -0,0 +1,12 @@
+"""
+- install VS Code python extension Microsoft
+- install Black Formatter
+- Crtl+Shift+P: > Python: choose interpreter
+
+"""
+
+name = "John Doe"
+print(name)
+
+greeting = "Hello World"
+print(greeting)
