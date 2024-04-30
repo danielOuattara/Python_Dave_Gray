be13d153238341cbbb30a79c250f3258ab3cfa78
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-30 23:30:27 +0200

    03 operators

---
+++ 03_operators/script.py
@@ -0,0 +1,21 @@
+"""
+Python divides the operators in the following groups:
+     > Arithmetic operators
+     > Assignment operators
+     > Comparison operators
+     > Logical operators
+     > Identity operators
+     > Membership operators
+     > Bitwise operators
+"""
+
+quantity = 42
+print("")
+
+if quantity > 10:
+    print("Greater")
+else:
+    print("Lower")
+
+# ternary equivalent
+print("Greater than 10") if quantity > 10  else print("Lower than 10")
