bf3e4ccf66c95f6d99422d03df9d633c367ecc24
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2025-01-21 22:06:16 +0100

    05 user input and control flow: logic OR file name updated

--- 05_user_input_and_control_flow/rps_script_1.py
+++ 05_user_input_and_control_flow/rps_script_1.py
@@ -1,37 +1,38 @@
+"""RPS simple game, version 1"""
+
 import sys
 import random
 from enum import Enum
 
 
-class RPS(Enum):
+class EnumRPS(Enum):
+    """Class representing Rock Paper Scissor enumeration value"""
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
 
 
-# print(RPS(2))
-# print(RPS.ROCK)
-# print(RPS['ROCK'])
-# print(RPS.ROCK.value)
-# print(RPS.ROCK.name)
+# print(EnumRPS(2))
+# print(EnumRPS.ROCK)
+# print(EnumRPS['ROCK'])
+# print(EnumRPS.ROCK.value)
+# print(EnumRPS.ROCK.name)
 # sys.exit()
 
-print("")
 
 player_choice = 0
 try:
     player_choice = int(
-        input("Enter your choice...\n1.Rock  \n2.Paper \n3.Scissor \n\n"))
+        input("\nEnter your choice...\n1.Rock  \n2.Paper \n3.Scissor \n\n"))
 
-except:
+except ValueError:
     if player_choice < 1 or player_choice > 3:
         sys.exit("Invalid input. Please enter 1, 2 or 3")
 
 computer_choice = int(random.choice("123"))
 
-print("You chose: " + str(RPS(player_choice).name) + ".")
-print("Computer chose: " + str(RPS(computer_choice).name) + ".")
-print("")
+print("You chose: " + str(EnumRPS(player_choice).name) + ".")
+print("Computer chose: " + str(EnumRPS(computer_choice).name) + ".\n")
 
 if player_choice == 1 and computer_choice == 3:
     print("you win !")
